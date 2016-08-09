d36b0aa37ff3b7e24fc2306817177c5325cdad2b
Jihdeh <jide.b.tade@gmail.com>
2016-08-09 01:24:03 +0100

    try allowed method

--- app/query-routes.js
+++ app/query-routes.js
@@ -47,7 +47,7 @@ function* webhook() {
     //
     // You must send back a 200, within 20 seconds, to let us know you've 
     // successfully received the callback. Otherwise, the request will time out.
-    this.status = 200;
+    this.body = 200;
   }
 }
 
