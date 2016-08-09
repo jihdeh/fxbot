6fc1c24bfbae95e62f9c973610ecc61a23eabc47
Jihdeh <jide.b.tade@gmail.com>
2016-08-09 01:13:45 +0100

    fix typo

--- app/query-routes.js
+++ app/query-routes.js
@@ -47,8 +47,8 @@ function* webhook() {
     //
     // You must send back a 200, within 20 seconds, to let us know you've 
     // successfully received the callback. Otherwise, the request will time out.
-  }
     this.status = 200;
+  }
 }
 
 
