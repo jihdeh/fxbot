b8b8bf73f3fd6ba58dc5b3d2f48a7f59d23d93f0
Jihdeh <jide.b.tade@gmail.com>
2016-08-21 10:02:38 +0100

    Upload json api to bin

--- app/config/web-scraper.js
+++ app/config/web-scraper.js
@@ -2,6 +2,7 @@ import request from "request";
 import cheerio from "cheerio";
 import fs from "fs";
 
+let API_BASE = "https://api.myjson.com/bins/186hf";
 let url = "http://abokifx.com";
 
 function scrape() {
@@ -25,10 +26,46 @@ function scrape() {
         };
 
         fs.writeFileSync(`./app/util/rates.json`, JSON.stringify(result, null, 2), { encoding: 'utf8' });
+        try {
+          request.put({url: API_BASE, body: result, json: true}, function(error, response, body) {
+            if (error) {
+              return console.error('upload failed:', error);
+            }
+            console.log('Upload successful!  Server responded with:', body);
+          });
+        } catch (e) {
+          console.log("error occured sending json", e);
+        }
       });
     }
   });
-
 }
 
-export default scrape;
+function westernUnionScrape() {
+  request(url, function(error, response, html) {
+    if (!error) {
+
+      let $ = cheerio.load(html);
+      $(".entry-content table tbody tr").next().next().next().filter(function() {
+        let data = $(this);
+        let recentData = data.contents().map(function(i, el) {
+          return $(this).html();
+        }).get();
+        console.log(recentData[1])
+          // let USD = $(recentData[9]).text();
+          // let GBP = $(recentData[10]).text();
+          // let EUR = $(recentData[11]).text();
+
+        // const result = {
+        //   "usd": USD,
+        //   "gbp": GBP,
+        //   "eur": EUR
+        // };
+
+        // fs.writeFileSync(`./app/util/rates.json`, JSON.stringify(result, null, 2), { encoding: 'utf8' });
+      });
+    }
+  });
+}
+
+export default { scrape, westernUnionScrape }
