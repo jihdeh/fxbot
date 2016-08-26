318f11d69d2d4328d0fcf66a05dddb8f11ed8770
Jihdeh <jide.b.tade@gmail.com>
2016-08-26 10:10:11 +0100

    quick hack methodology

--- app/config/text-responder.js
+++ app/config/text-responder.js
@@ -4,30 +4,11 @@ import numbro from "numbro"
 import transform from "../util/transform";
 import fetchRates from "./web-scraper";
 
-let ratez, rates;
-fetchRates.getRates(data => {
-  ratez = data;
-  return ratez;
-});
-
-if (ratez) {
-  rates = `Todays Rates \n\nUSD => ${ratez.usd} \nGBP => ${ratez.gbp} 
-EUR => ${ratez.eur} \n\nCURRENCY => BUY / SELL \nData pulled from http://abokifx.com`;
 
 
-  fx.base = "NGN";
-  fx.settings = { from: "NGN" };
-  fx.rates = {
-    "USD": ratez.usd.split(" ")[0],
-    "GBP": ratez.gbp.split(" ")[0],
-    "EUR": ratez.eur.split(" ")[0],
-    "NGN": 1
-  }
-}
 
-setTimeout(() => {
-  console.log(ratez);
-}, 5000)
+
+
 
 function generate(text) {
   let newText = text.split(" ");
@@ -56,27 +37,43 @@ function generate(text) {
 }
 
 function listener(text) {
-  text = text.toLowerCase();
-  if (text === "rates" || text === "rate") {
-    return rates;
-  } else {
-    const response = generate(text);
-    let value = "";
-    if (response.convertCurrencyFrom && response.convertCurrencyTo) {
-      value = fx.convert(response.amount, {
-        to: response.convertCurrencyFrom,
-        from: response.convertCurrencyTo
-      });
-    } else if (response.convertCurrencyFrom) {
-      value = fx.convert(response.amount, {
-        to: response.convertCurrencyFrom
-      });
+  fetchRates.getRates(data => {
+
+    const ratez = data;
+    const rates = `Todays Rates \n\nUSD => ${ratez.usd} \nGBP => ${ratez.gbp} 
+EUR => ${ratez.eur} \n\nCURRENCY => BUY / SELL \nData pulled from http://abokifx.com`;
+
+    text = text.toLowerCase();
+    if (text === "rates" || text === "rate") {
+      return rates;
     } else {
-      return "Sorry there was a problem processing your command \nPlease check the commands on the facebook page \n \
+      const response = generate(text);
+      let value = "";
+      fx.base = "NGN";
+      fx.settings = { from: "NGN" };
+      fx.rates = {
+        "USD": ratez.usd.split(" ")[0],
+        "GBP": ratez.gbp.split(" ")[0],
+        "EUR": ratez.eur.split(" ")[0],
+        "NGN": 1
+      }
+      if (response.convertCurrencyFrom && response.convertCurrencyTo) {
+        value = fx.convert(response.amount, {
+          to: response.convertCurrencyFrom,
+          from: response.convertCurrencyTo
+        });
+      } else if (response.convertCurrencyFrom) {
+        value = fx.convert(response.amount, {
+          to: response.convertCurrencyFrom
+        });
+
+      } else {
+        return "Sorry there was a problem processing your command \nPlease check the commands on the facebook page \n \
       @ https://facebook.com/fxbot0";
+      }
+      return numbro(value).format('0,0') + " naira, is what you will get on parallel market";
     }
-    return numbro(value).format('0,0') + " naira, is what you will get on parallel market";
-  }
+  });
 }
 
 function sendTextMessage(recipientId, messageText) {
---
+++ cron/getrates.js
@@ -0,0 +1,10 @@
+// function getRates(cb) {
+//   request.get({ url: API_BASE, json: true }, function(error, response, body) {
+//     if (error) {
+//       return console.error('upload failed:', error);
+//     }
+//     cb(body);
+//   });
+// }
+
+// module.exports = getRates;
