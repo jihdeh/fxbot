f6270c06d9087db73092bb5eefcef4a2be620765
Jihdeh <jide.b.tade@gmail.com>
2016-08-31 08:31:29 +0100

    More format fixing

--- app/publish-bot.js
+++ app/publish-bot.js
@@ -93,7 +93,9 @@ function receivedPostback(event) {
 
   if (payload === "PAYLOAD_HELP" || payload === "PAYLOAD_GETTING_STARTED") {
     postbackMessage = `Hello there welcome to NAIRABOT! \nYou can use commands like
-    \n"rates"\n"convert 100 dollars"\n"westy"(for western union rates)\n"wu convert 400 pounds"
+    \n"rates"\n"convert 100 dollars(to convert at parallel market rates)"
+    \n"westy"(for western union rates)\n"wu convert 400 pounds"
+    \n"cbn rates(for cbn exchange rates)"
     \nplease check our page for more commands you can use`;
     sendTextMessage(senderID, postbackMessage, "help");
   } else if(payload === "PAYLOAD_PARALLEL") {
