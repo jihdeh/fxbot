c56d51f4ce689ffdedd907f4fc6a1424f8887163
Jihdeh <jide.b.tade@gmail.com>
2016-08-31 08:28:29 +0100

    fix help message

--- app/config/text-responder.js
+++ app/config/text-responder.js
@@ -90,8 +90,13 @@ listener("cbn rates");
 async function sendTextMessage(recipientId, messageText, postback) {
   let response;
   messageText = messageText.toLowerCase();
-  if (postback === "help" || messageText === "help") {
+  if (postback === "help") {
     response = messageText
+  } else if (messageText === "help") {
+    response = `Hello there welcome to NAIRABOT! \nYou can use commands like
+    \n"rates"\n"convert 100 dollars"\n"westy"(for western union rates)\n"wu convert 400 pounds"
+    \n"cbn rates"
+    \nplease check our page for more commands you can use`;
   } else {
     response = await listener(messageText);
   }
