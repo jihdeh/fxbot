51cd1b8f5413bb27d2fe2a692b65f1e12cdc7603
Jihdeh <jide.b.tade@gmail.com>
2016-08-31 08:30:11 +0100

    fix formatting

--- app/config/text-responder.js
+++ app/config/text-responder.js
@@ -94,8 +94,9 @@ async function sendTextMessage(recipientId, messageText, postback) {
     response = messageText
   } else if (messageText === "help") {
     response = `Hello there welcome to NAIRABOT! \nYou can use commands like
-    \n"rates"\n"convert 100 dollars"\n"westy"(for western union rates)\n"wu convert 400 pounds"
-    \n"cbn rates"
+    \n"rates"\n"convert 100 dollars"
+    \n"westy"(for western union rates)\n"wu convert 400 pounds"
+    \n"cbn rates(for cbn exchange rates)"
     \nplease check our page for more commands you can use`;
   } else {
     response = await listener(messageText);
