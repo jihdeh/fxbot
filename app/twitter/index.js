b8f91312ace6e728a5ba6de228834a41c41f92b7
Jihdeh <jide.b.tade@gmail.com>
2016-09-02 08:23:24 +0100

    change default message

--- app/twitter/index.js
+++ app/twitter/index.js
@@ -5,7 +5,8 @@ const Tweet = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000
 });
 
 var stream = Tweet.stream('statuses/filter', { track: ['@nairabot'] });
@@ -15,6 +16,11 @@ function tweetEvent(tweet) {
   console.log(tweet, "=====================teswwett");
   // const rates = await report("rates");
   // console.log(rates)
+  // let rates;
+  // async function getRates() {
+  //   rates = await report("rates");
+  //   return rates;
+  // }
 
     // Who sent the tweet?
     var name = tweet.user.screen_name;
@@ -27,7 +33,7 @@ function tweetEvent(tweet) {
     // var txt = txt.replace(/@myTwitterHandle/g, "");
 
     // Start a reply back to the sender
-    var reply = "You mentioned me! @" + name + ' ' + 'You are super cool!';
+    var reply = "@" + name + ' ' + 'You are super cool!';
     var params             = {
                               status: reply,
                               in_reply_to_status_id: nameID
