0591998e5ce398cb453c5b79da6f6a09b0f2aee8
Jihdeh <jide.b.tade@gmail.com>
2016-09-02 12:32:10 +0100

    for now publish only hourly rates

--- app/twitter/index.js
+++ app/twitter/index.js
@@ -7,45 +7,54 @@ const Tweet = new Twit({
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60*1000
+  timeout_ms: 60 * 1000
 });
 
-var stream = Tweet.stream('statuses/filter', { track: ['@nairabot'] });
-stream.on('tweet', tweetEvent);
-
-function tweetEvent(tweet) {
+export default async function publishRates() {
   const generateShortId = "#"+shortId.generate();
-  console.log(tweet, "=====================teswwett");
-  // const rates = await report("rates");
-  // console.log(rates)
-  // let rates;
-  // async function getRates() {
-  //   rates = await report("rates");
-  //   return rates;
-  // }
+  const rates = await report("rates");
+  const construct = rates + "\n"+generateShortId;
+  // console.log(construct);
+  Tweet.post('statuses/update', { status: construct }, function(err, data, response) {
+    console.log(data)
+  })
+}
 
-    // Who sent the tweet?
-    var name = tweet.user.screen_name;
-    // What is the text?
-    // var txt = tweet.text;
-    // the status update or tweet ID in which we will reply
-    var nameID  = tweet.id_str;
+// var stream = Tweet.stream('statuses/filter', { track: ['@nairabot'] });
+// stream.on('tweet', tweetEvent);
 
-     // Get rid of the @ mention
-    // var txt = txt.replace(/@myTwitterHandle/g, "");
+// function tweetEvent(tweet) {
+//   console.log(tweet, "=====================teswwett");
+//   // const rates = await report("rates");
+//   // console.log(rates)
+//   // let rates;
+//   // async function getRates() {
+//   //   rates = await report("rates");
+//   //   return rates;
+//   // }
 
-    // Start a reply back to the sender
-    var reply = "@" + name + ' ' + `You are super cool!\n${generateShortId}`;
-    var params             = {
-                              status: reply,
-                              in_reply_to_status_id: nameID
-                             };
+//     // Who sent the tweet?
+//     var name = tweet.user.screen_name;
+//     // What is the text?
+//     // var txt = tweet.text;
+//     // the status update or tweet ID in which we will reply
+//     var nameID  = tweet.id_str;
 
-    Tweet.post('statuses/update', params, function(err, data, response) {
-      if (err !== undefined) {
-        console.log(err);
-      } else {
-        console.log('Tweeted: ' + params.status);
-      }
-    })
-};
+//      // Get rid of the @ mention
+//     // var txt = txt.replace(/@myTwitterHandle/g, "");
+
+//     // Start a reply back to the sender
+//     var reply = "@" + name + ' ' + `You are super cool!\n${generateShortId}`;
+//     var params             = {
+//                               status: reply,
+//                               in_reply_to_status_id: nameID
+//                              };
+
+//     Tweet.post('statuses/update', params, function(err, data, response) {
+//       if (err !== undefined) {
+//         console.log(err);
+//       } else {
+//         console.log('Tweeted: ' + params.status);
+//       }
+//     })
+// };
--- cron/scrape-worker.js
+++ cron/scrape-worker.js
@@ -1,6 +1,10 @@
 import moment from "moment";
 import schedule from "./rescrape-schedule";
 import scraper from "../app/config/web-scraper";
+import twitterPublish from "../app/twitter";
+
 
 console.log("Updating once every", moment.duration(schedule.update).humanize());
 setInterval(scraper.scrape, schedule.update);
+twitterPublish();
+setInterval(twitterPublish, schedule.update);
