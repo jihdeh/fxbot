60854d679966fc121b1e8ab89094e5ed92e771c5
Jihdeh <jide.b.tade@gmail.com>
2016-09-03 15:50:00 +0100

    Hotfix path problem

--- cron/scrape-worker.js
+++ cron/scrape-worker.js
@@ -1,7 +1,6 @@
 import moment from "moment";
 import schedule from "./rescrape-schedule";
 import scraper from "../app/config/web-scraper";
-import twitterPublish from "../app/twitter";
 
 
 console.log("Updating Facebook once every", moment.duration(schedule.update).humanize());
