776efb76cac002a4838eb55fc76acbe04dde5110
Jihdeh <jide.b.tade@gmail.com>
2016-09-09 01:58:43 +0100

    display shorter msg for twitter in console

--- worker/twitter/twitter.js
+++ worker/twitter/twitter.js
@@ -13,7 +13,7 @@ const Tweet = new Twit({
   const constructTweet = rates;
   try {
     const tweetUpdate = await Tweet.post('statuses/update', { status: constructTweet });
-    console.log(tweetUpdate.data);
+    console.log(tweetUpdate.data.created_at);
   } catch (e) {
     console.log(e, "error occured posting tweet update");
   }
