aa44f67d7171cc4c83c409504f27fac395fd4185
Jihdeh <jide.b.tade@gmail.com>
2016-09-09 03:00:42 +0100

    what comes from the promise

--- app/config/notification/notify.js
+++ app/config/notification/notify.js
@@ -3,39 +3,42 @@ import NotifyModel from "./notify-model";
 
 function findUserId(id) {
   const response = NotifyModel.find({ recipient: id }).lean().exec();
-  console.log(response, "response finding");
-  if (Object.keys(response).length !== 0) {
-    return {
-      type: "postback",
-      title: "Disable Notification",
-      payload: "PAYLOAD_NOTIFY_ENABLE"
+  response.then(doc => {
+    console.log(doc, "----doc")
+    if (doc.length > 0) {
+      return {
+        type: "postback",
+        title: "Disable Notification",
+        payload: "PAYLOAD_NOTIFY_ENABLE"
+      }
+    } else {
+      return {
+        type: "postback",
+        title: "Enable Notification",
+        payload: "PAYLOAD_NOTIFY_ENABLE"
+      }
     }
-  } else {
-    return {
-      type: "postback",
-      title: "Enable Notification",
-      payload: "PAYLOAD_NOTIFY_ENABLE"
-    }
-  }
+  })
 }
 
 function notify(recipientId) {
   const actionData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: "𝐅𝐗 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 \nYou'll receive market updates throughout the day every 3 hours.",
-          buttons: [findUserId(recipientId)]
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: "𝐅𝐗 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 \nYou'll receive market updates throughout the day every 3 hours.",
+            buttons: [findUserId(recipientId)]
+          }
         }
       }
     }
-  }
-  // callSendAPI(actionData);
+    console.log(JSON.stringify(actionData.message.attachment.payload));
+    // callSendAPI(actionData);
 }
 
 
