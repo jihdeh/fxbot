7e29f715fd7c28cc13fa4ffb8bf34c88b4a813e2
Jihdeh <jide.b.tade@gmail.com>
2016-09-09 04:48:33 +0100

    properly format rates message

--- cron/fb-notifier.js
+++ cron/fb-notifier.js
@@ -1,8 +1,8 @@
 import cron from "node-cron";
 import fbBroadcast from "../worker/facebook";
 
-const task = cron.schedule('*/2 * * * *', function() {
-  console.log('running a task 1 minute');
+const task = cron.schedule('* * * * *', function() {
+  console.log('running task every 3 hours');
   fbBroadcast();
 }, false);
 task.start();
--- worker/facebook/text-responder.js
+++ worker/facebook/text-responder.js
@@ -8,8 +8,12 @@ async function listener(text) {
   const parallel = wordAI.generalRates.includes(text);
 
   if (parallel) {
-    const endRatesResult = `Todays rates on ${moment().format("MMMM Do YYYY")} at ${moment().utcOffset(1).format("h:mm:ss a")} \n\nUSD => ${rates.parallel.usd} \nGBP => ${rates.parallel.gbp} 
-EUR => ${rates.parallel.eur} \n\nCURRENCY => BUY / SELL`;
+    const endRatesResult = `PARALLEL MKT. RATES at ${moment().utcOffset(1).format("h:mm:ss a")} \n\nUSD => ${rates.parallel.usd} \nGBP => ${rates.parallel.gbp} 
+EUR => ${rates.parallel.eur} \n\nCURRENCY => BUY / SELL \n\n-------------------------------
+\nCBN RATES  \n\nUSD => ${rates.cbn.usd} \nGBP => ${rates.cbn.gbp} 
+EUR => ${rates.cbn.eur} \n\n-------------------------------
+\nWESTERN UNION(Receiving)  \n\nUSD => ${rates.wu.usd} \nGBP => ${rates.wu.gbp} 
+EUR => ${rates.wu.eur}`;
     return endRatesResult;
   }
 }
