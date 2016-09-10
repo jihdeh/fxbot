e2c4315134308126ea75a878f3e167645d2c10a2
Jihdeh <jide.b.tade@gmail.com>
2016-09-10 09:51:50 +0100

    Reduce length of message param for fb

--- worker/facebook/text-responder.js
+++ worker/facebook/text-responder.js
@@ -4,9 +4,9 @@ import moment from "moment";
 async function listener() {
   const rates = await Rates();
   const endRatesResult = `PARALLEL MKT. RATES at ${moment().utcOffset(1).format("h:mm:ss a")} \n\nUSD => ${rates.parallel.usd} \nGBP => ${rates.parallel.gbp} 
-EUR => ${rates.parallel.eur} \n\nCURRENCY => BUY / SELL \n\n-------------------------------
+EUR => ${rates.parallel.eur} \n\nCURRENCY => BUY / SELL \n\n---------------
 \nCBN RATES  \n\nUSD => ${rates.cbn.usd} \nGBP => ${rates.cbn.gbp} 
-EUR => ${rates.cbn.eur} \n\n-------------------------------
+EUR => ${rates.cbn.eur} \n\n---------------
 \nWESTERN UNION(Receiving)  \n\nUSD => ${rates.wu.usd} \nGBP => ${rates.wu.gbp} 
 EUR => ${rates.wu.eur}`;
   return endRatesResult;
