e91c545cf115bf14d70417a4860d4853515ee861
Jihdeh <jide.b.tade@gmail.com>
2016-09-30 13:47:19 +0100

    modularixe and split messages

--- app/config/session/session.js
+++ app/config/session/session.js
@@ -1,29 +1,33 @@
 import callSendAPI from "../send-requests";
 import SessionModel from "../../model/session";
 import AbokiModel from "../../model/aboki";
+import {get} from "lodash";
+
+function servicify(id, text) {
+  const actionData = {
+    recipient: {
+      id: id
+    },
+    message: {
+      text: text
+    }
+  };
+  callSendAPI(actionData);
+};
 
 async function findSessionData(sessionID, abokiID) {
   const findSession = await SessionModel.findOne({ sessionId: sessionID }).lean().exec();
-  if (findSession.aboki) {
-    console.log("Sessin taken");
-    return "Sorry this session is already taken, you will be notified when there's another";
-  } else {
+  if (findSession.aboki && findSession.aboki === abokiID) {
+    servicify(id, "Sorry this session is already taken by you, if you want to cancel, just send > aboki cancel");
+  } else if(findSession.aboki) {
+    servicify(id, "Sorry this session is already taken, you will be notified when there's another");
+  }
+  else {
     SessionModel.findOneAndUpdate({ sessionId: sessionID }, { aboki: abokiID }, { upsert: true }, (err, doc) => {});
     AbokiModel.findOneAndUpdate({ abokiID: abokiID }, { inSession: true }, { upsert: true }, (err, doc) => {});
-    console.log("ahah", abokiID, findSession)
     let IDs = [abokiID, findSession.requester];
-    console.log(IDs, "00---------")
     for (let id of IDs) {
-      console.log("the ids ohhhh", id);
-      const actionData = {
-        recipient: {
-          id: id
-        },
-        message: {
-          text: "You are now connected, please send a message"
-        }
-      };
-      callSendAPI(actionData)
+      servicify(id, "You are now connected, please send a message")
     }
   }
 }
