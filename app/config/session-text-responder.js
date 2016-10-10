9936205254e88f69761414251b62f9855fe77ad3
Jihdeh <jide.b.tade@gmail.com>
2016-10-10 22:32:39 +0100

    fix bug set message to send to recipient instead of aboki

--- app/config/session-text-responder.js
+++ app/config/session-text-responder.js
@@ -1,36 +1,22 @@
 import callSendAPI from "./send-requests";
 import wordAI from "../util/word-ai";
 import Request from "../bid-exchange/request";
-import {forEach} from "lodash";
+import { forEach } from "lodash";
 
 
 function isContains(word, substr) {
   let nevalue = false;
-  forEach(substr, (value) => { 
+  forEach(substr, (value) => {
     if (word.indexOf(value !== "" && value) > -1) return nevalue = true;
   });
   return nevalue;
 }
 
 String.prototype.capitalizeFirstLetter = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+  return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-
-async function sendSessionMessage(recipientId, messageText) {
-  messageText = messageText.toLowerCase();
-  let newText = messageText;
-  console.log(isContains(messageText, wordAI.cancelRequest));
-  if(isContains(messageText, wordAI.cancelRequest)) {
-    newText = await Request.RemoveRequest(recipientId);
-    console.log("yesss",newText);
-  }
-  // switch (messageText) {
-  //   case isContains(messageText, wordAI.cancelRequest):
-  //     break;
-  //   default: 
-  //     return;
-  // }
+function sendSessionMessage(recipientId, newText) {
   const messageData = {
     recipient: {
       id: recipientId
@@ -44,6 +30,23 @@ async function sendSessionMessage(recipientId, messageText) {
   } catch (error) {
     console.log("An error occured");
   }
+}
+
+function cancelRequest(otherpartyId) {
+  let newText = await Request.RemoveRequest(recipientId);
+  sendSessionMessage(otherpartyId, newText)
+}
+
+
+async function sendSessionMessage(recipientId, messageText, otherpartyId) {
+  messageText = messageText.toLowerCase();
+  let newText = messageText;
+  console.log(isContains(messageText, wordAI.cancelRequest));
+  if (isContains(messageText, wordAI.cancelRequest)) {
+    cancelRequest(otherpartyId);
+    return;
+  }
+  sendSessionMessage(recipientId, messageText);
 }
 
 export default sendSessionMessage
